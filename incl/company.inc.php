008f8aff47de400ac472c0ac3aefe5a9e57ba425
Andreas Beer <andreasbeer@gmx.com>
2016-12-03 11:43:55 +0100

    company refaktorisiert

--- incl/company.inc.php
+++ incl/company.inc.php
@@ -12,37 +12,13 @@
 
 <?php
 include_once PATH_FILE_FUNCTIONS;
+
+$href  = $_SERVER['PHP_SELF'] . '?c=' . $data['id'];
+$name  = $data['Name'];
+$class = isActive($siteData['id'], $data['id']);
+
 ?>
 
-<nav>
-
-  <!-- Suche -->
-  <form class="navbar-form" role="search">
-    <div class="form-group">
-      <div class="input-group">
-        <input type="text" class="form-control" placeholder="<?php echo TEXT_MAIN_GUI_SEARCH_PLACEHOLDER;?>">
-        <span class="input-group-btn">
-          <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
-        </span>
-      </div>
-    </div>
-  </form>
-
-  <!-- Navigation -->
-  <ul class="nav nav-pills nav-stacked">
-
-    <?php
-//        <li class = "active"><a href = "#">###FILMGESELLSCHAFT1###</a></li>
-
-    foreach ($dbCompanies as $data) {
-
-      $href = $_SERVER['PHP_SELF'] . '?c=' . $data['id'];
-
-      echo '<li class="' . isActive($siteData['id'], $data['id']) . '">';
-      echo '<a href = "' . $href . '">' . $data['Name'] . '</a>';
-      echo '</li>';
-    }
-    ?>
-  </ul>
-
-</nav>
+<li class="<?php echo $class; ?>">
+  <a href = "<?php echo $href; ?>"><?php echo $name; ?></a>
+</li>
--- index.php
+++ index.php
@@ -17,7 +17,7 @@ require_once PATH_FILE_DBCONNECT;
 
 <?php
 
-function getData () {
+function getData() {
 
   $type = '';
   $id   = '';
@@ -33,9 +33,7 @@ function getData () {
     $data['id']    = $id;
     $data['title'] = sprintf(TEXT_MAIN_GUI_HEADLINE_GENRE, $category);
     $data['data']  = getDataFromDB(sql_select_moviesByGenreId($id));
-  }
-  
-  elseif (!empty($_GET['c']) && is_numeric($_GET['c'])) {
+  } elseif (!empty($_GET['c']) && is_numeric($_GET['c'])) {
     $type      = 'c';
     $id        = $_GET[$type];
     $title_arr = getDataFromDB(sql_select_companyByID($id));
@@ -45,9 +43,7 @@ function getData () {
     $data['id']    = $id;
     $data['title'] = sprintf(TEXT_MAIN_GUI_HEADLINE_COMPANY, $category);
     $data['data']  = getDataFromDB(sql_select_moviesByCompanyId($id));
-  }
-  
-  else {
+  } else {
     $data['id']    = $id;
     $data['title'] = TEXT_MAIN_GUI_HEADLINE_DEFAULT;
     $data['data']  = getDataFromDB(sql_select_moviesNew10());
@@ -69,28 +65,55 @@ $title = htmlspecialchars($siteData['title']);
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <title><?php echo $title;?></title>
-    <link rel="stylesheet" href="<?php echo PATH_FILE_STYLE_MAIN;?>" />
+    <title><?php echo $title; ?></title>
+    <link rel="stylesheet" href="<?php echo PATH_FILE_STYLE_MAIN; ?>" />
   </head>
 
   <body>
 
-    <?php
-    session_start();
+<?php
+session_start();
 
-    if (isset($_GET['adminbar']) || !empty($_SESSION['login']) == true) {
-      include PATH_FILE_ADMINBAR;
-    }
-    ?>
+if (isset($_GET['adminbar']) || !empty($_SESSION['login']) == true) {
+  include PATH_FILE_ADMINBAR;
+}
+?>
 
-    <?php include PATH_FILE_HEADER;?>
+    <?php include PATH_FILE_HEADER; ?>
 
     <main class="main container">
       <div class="row">
 
         <div class="col-sm-4 col-md-3 sidebar">
 
-          <?php include PATH_FILE_COMPANY;?>
+          <nav>
+
+            <!-- Suche -->
+            <form class="navbar-form" role="search">
+              <div class="form-group">
+                <div class="input-group">
+                  <input type="text" class="form-control" placeholder="<?php echo TEXT_MAIN_GUI_SEARCH_PLACEHOLDER; ?>">
+                  <span class="input-group-btn">
+                    <button type="button" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+                  </span>
+                </div>
+              </div>
+            </form>
+
+            <!-- Navigation -->
+            <ul class="nav nav-pills nav-stacked">
+
+              <?php
+              //        <li class = "active"><a href = "#">###FILMGESELLSCHAFT1###</a></li>
+
+              foreach ($dbCompanies as $data) {
+                include PATH_FILE_COMPANY;
+              }
+              ?>
+              
+            </ul>
+
+          </nav>
 
           <!-- Container für weitere Links -->
 <!--          <aside class="links panel panel-default hidden-xs">
@@ -109,25 +132,25 @@ $title = htmlspecialchars($siteData['title']);
         <div class="col-sm-8 col-md-9">
 
           <div class="well well-sm">
-            <?php
-            echo sprintf(TEXT_MAIN_GUI_MSG_FILMFOUND, count($siteData['data']));
-            ?>
+<?php
+echo sprintf(TEXT_MAIN_GUI_MSG_FILMFOUND, count($siteData['data']));
+?>
           </div>
 
-          <?php
-          foreach ($siteData['data'] as $data) {
-            include PATH_FILE_FILM;
-          }
-          ?>
+            <?php
+            foreach ($siteData['data'] as $data) {
+              include PATH_FILE_FILM;
+            }
+            ?>
 
         </div>
 
       </div>
     </main>
-    <?php include PATH_FILE_FOOTER;?>
+<?php include PATH_FILE_FOOTER; ?>
 
-    <script src="<?php echo PATH_FILE_JS_JQUERY;?>" type="text/javascript"></script>
-    <script src="<?php echo PATH_FILE_JS_BOOTSTRAP;?>" type="text/javascript"></script>
+    <script src="<?php echo PATH_FILE_JS_JQUERY; ?>" type="text/javascript"></script>
+    <script src="<?php echo PATH_FILE_JS_BOOTSTRAP; ?>" type="text/javascript"></script>
 
   </body>
 </html>
